a859473887f2030f85b6fe450defb3ba45fce1bd
Vlademir <vlademir.santos@smarthis.com.br>
2021-07-27 23:16:11 -0300

    feat: dollar equality

--- src/dollar.ts
+++ src/dollar.ts
@@ -7,4 +7,8 @@ export class Dollar {
   times (multiplier: number): Dollar {
     return new Dollar(this.amount * multiplier)
   }
+
+  equals (other: Dollar): boolean {
+    return this.amount === other.amount
+  }
 }
