f2ace2e692c8c25dc6574397f8b931623c4f74e9
Vlademir <vlademir.santos@smarthis.com.br>
2021-07-27 23:16:27 -0300

    test: dollar equality

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -1,11 +1,16 @@
 import { Dollar } from './dollar'
 
 describe('Dollar', () => {
-  test('should return 10 when 5 dollars is multiplied by 2', () => {
+  it('should return 10 when 5 dollars is multiplied by 2', () => {
     const five = new Dollar(5)
-    var prod: Dollar = five.times(2)
+    let prod: Dollar = five.times(2)
     expect(prod.amount).toEqual(10)
     prod = five.times(3)
     expect(prod.amount).toEqual(15)
+  })
+
+  it('should return true when dollars of the same value are compared', () => {
+    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy()
+    expect(new Dollar(5).equals(new Dollar(10))).toBeFalsy()
   })
 })
