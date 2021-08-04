fd9037aa84bb3e09225f8e017b7ba0c70881893c
Vlademir <vlademir.santos@smarthis.com.br>
2021-08-03 22:44:36 -0300

    test: euro equality

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -2,20 +2,22 @@ import { Dollar } from './dollar'
 import { Euro } from './euro'
 
 describe('Money', () => {
-  it('should return 10 when 5 dollars is multiplied by 2 and 15 multiplied by 3', () => {
+  it('should correctly handle dollar multiplication', () => {
     const five = new Dollar(5)
     expect(five.times(2).equals(new Dollar(10))).toBeTruthy()
     expect(five.times(3).equals(new Dollar(15))).toBeTruthy()
   })
 
-  it('should return 10 when 5 Euros is multiplied by 2 and 15 multiplied by 3', () => {
+  it('should correctly handle euro multiplication', () => {
     const five = new Euro(5)
     expect(five.times(2).equals(new Euro(10))).toBeTruthy()
     expect(five.times(3).equals(new Euro(15))).toBeTruthy()
   })
 
-  it('should return true when dollars of the same value are compared', () => {
+  it('should correctly handle money equality', () => {
     expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy()
     expect(new Dollar(5).equals(new Dollar(10))).toBeFalsy()
+    expect(new Euro(5).equals(new Euro(5))).toBeTruthy()
+    expect(new Euro(5).equals(new Euro(10))).toBeFalsy()
   })
 })
