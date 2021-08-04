1cee02edb6d899f241029b76f49159ac9e8f099c
Vlademir <vlademir.santos@smarthis.com.br>
2021-08-03 22:17:51 -0300

    "test:make amount private"

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -3,10 +3,8 @@ import { Dollar } from './dollar'
 describe('Dollar', () => {
   it('should return 10 when 5 dollars is multiplied by 2', () => {
     const five = new Dollar(5)
-    let prod: Dollar = five.times(2)
-    expect(prod.amount).toEqual(10)
-    prod = five.times(3)
-    expect(prod.amount).toEqual(15)
+    expect(five.times(2).equals(new Dollar(10))).toBeTruthy()
+    expect(five.times(3).equals(new Dollar(15))).toBeTruthy()
   })
 
   it('should return true when dollars of the same value are compared', () => {
