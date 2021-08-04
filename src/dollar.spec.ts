b54358301332cadb625a68502f9b420e970e5d78
Vlademir <vlademir.santos@smarthis.com.br>
2021-08-03 22:22:08 -0300

    test:description update

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -1,7 +1,7 @@
 import { Dollar } from './dollar'
 
 describe('Dollar', () => {
-  it('should return 10 when 5 dollars is multiplied by 2', () => {
+  it('should return 10 when 5 dollars is multiplied by 2 and 15 multiplied by 3', () => {
     const five = new Dollar(5)
     expect(five.times(2).equals(new Dollar(10))).toBeTruthy()
     expect(five.times(3).equals(new Dollar(15))).toBeTruthy()
