6c28e5ed56ec8b36ff4599f058ad08a78465b054
Vlademir <vlademir.santos@smarthis.com.br>
2021-08-03 23:29:19 -0300

    refactor: remove redundant classes

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -7,19 +7,10 @@ describe('Money', () => {
     expect(five.times(3).equals(Money.dollar(15))).toBeTruthy()
   })
 
-  it('should correctly handle euro multiplication', () => {
-    const five = Money.euro(5)
-    expect(five.times(2).equals(Money.euro(10))).toBeTruthy()
-    expect(five.times(3).equals(Money.euro(15))).toBeTruthy()
-  })
-
   it('should correctly handle money equality', () => {
     expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy()
     expect(Money.dollar(5).equals(Money.dollar(10))).toBeFalsy()
-    expect(Money.euro(5).equals(Money.euro(5))).toBeTruthy()
-    expect(Money.euro(5).equals(Money.euro(10))).toBeFalsy()
     expect(Money.euro(5).equals(Money.dollar(5))).toBeFalsy()
-    expect(new Money(5, 'USD').equals(Money.dollar(5))).toBeTruthy()
   })
 
   it('should correctly handle currencies', () => {
