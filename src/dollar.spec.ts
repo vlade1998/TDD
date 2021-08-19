ab9fca40039b962b307528cbcd54dcbc5ff4c535
Vlademir <vlademir.santos@smarthis.com.br>
2021-08-18 23:41:29 -0300

    test: get closer to abstraction

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -8,8 +8,8 @@ import { hasUncaughtExceptionCaptureCallback } from 'process'
 describe('Money', () => {
   it('should handle dollar multiplication', () => {
     const five = Money.dollar(5)
-    expect(five.times(2).equals(Money.dollar(10))).toBeTruthy()
-    expect(five.times(3).equals(Money.dollar(15))).toBeTruthy()
+    expect(five.times(2)).toEqual(Money.dollar(10))
+    expect(five.times(3)).toEqual(Money.dollar(15))
   })
 
   it('should handle money equality', () => {
@@ -61,5 +61,14 @@ describe('Money', () => {
 
   it('should handle identity rate', () => {
     expect(new Bank().rate('USD', 'USD')).toEqual(1)
+  })
+
+  it('should handle mixed addition', () => {
+    const fiveBucks: Expression = Money.dollar(5)
+    const tenEuros: Expression = Money.euro(10)
+    const bank = new Bank()
+    bank.addRate('EUR', 'USD', 2)
+    const result = bank.reduce(fiveBucks.plus(tenEuros), 'USD')
+    expect(result).toEqual(Money.dollar(10))
   })
 })
